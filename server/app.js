4d0e801f5892fe47afdfb82a951a13c6ae1111ce
hssanchez17 <hssanchez.17@est.ucab.edu.ve>
2019-12-07 08:41:48 +0100

    configuraciones listas

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan=require('morgan')
+const multer=require('multer')
+const path=require('path')
+const exphbs=require('express-handlebars')
+const app = express();
+
+
+//Settings
+app.set('views',path.join(__dirname,'views'));
+app.engine('.hbs',exphbs({
+  defaultLayout:'main',
+  layoutsDir: path.join(app.get('views'),'layaouts'),
+  partialsDir:path.join(app.get('views'),'partials'),
+  extname:'hbs'
+}))
+
+app.set('view engine','.hbs')
+
+//Middelwares
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.use(multer().single('image'))
+
+const storage=multer.diskStorage({
+  destination:path.join(__dirname,'public/uploads'),
+  filename:(req,file,cb)=>{
+    cb(null,new Date().getTime()+path.extname(file.originalname))//Encripta la info de la foto
+  }
+})
+
+app.use(multer({storage}).single('image'))
+app.set('puerto', process.env.PORT || 3000);
+app.use('/',require('./routes'))
+app.listen(app.get('puerto'), () => {
+  console.log('Example app listening on port'+ app.get('puerto'));
+});
